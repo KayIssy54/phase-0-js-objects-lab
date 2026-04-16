1c6c70ff41b657d46c68367014063ad4fecbc1b3
KayIssy54 <mwongerajoy4@gmail.com>
2026-04-16 14:39:25 +0300

    fix eerors

--- index.js
+++ index.js
@@ -1,51 +1,49 @@
-//Write your code here
+// attendee object
 let attendee = {
     attendeeId: "T001",
     name: "Alice Smith",
     event: "JavaScript Conference",
     ticketType: "VIP",
     ticketPrice: 150.00
-}
+};
 
-
+// 1. Log attendee name
 function logAttendeeName(attendee) {
     console.log(attendee.name);
 }
 
-
+// 2. Log ticket price
 function logTicketPrice(attendee) {
     console.log(attendee.ticketPrice);
 }
 
-
+// 3. Update ticket type
 function updateTicketType(attendee, newTicketType) {
     attendee.ticketType = newTicketType;
 }
 
-
+// 4. Update ticket price
 function updateTicketPrice(attendee, newTicketPrice) {
     attendee.ticketPrice = newTicketPrice;
 }
 
-
+// 5. Remove event property
 function removeEventProperty(attendee) {
     delete attendee.event;
 }
 
-
+// 6. Add checked-in property
 function addCheckedInProperty(attendee) {
     attendee.checkedIn = true;
 }
 
-
-
-//Needed for the tests to work. Don't modify
+// REQUIRED EXPORT (IMPORTANT FOR TESTS)
 module.exports = {
-  ...(typeof attendee !== 'undefined' && { attendee }),
-  ...(typeof logAttendeeName !== 'undefined' && { logAttendeeName }),
-  ...(typeof logTicketPrice !== 'undefined' && { logTicketPrice }),
-  ...(typeof updateTicketType !== 'undefined' && { updateTicketType }),
-  ...(typeof updateTicketPrice !== 'undefined' && { updateTicketPrice }),
-  ...(typeof removeEventProperty !== 'undefined' && { removeEventProperty }),
-  ...(typeof addCheckedInProperty !== 'undefined' && { addCheckedInProperty })
+    attendee,
+    logAttendeeName,
+    logTicketPrice,
+    updateTicketType,
+    updateTicketPrice,
+    removeEventProperty,
+    addCheckedInProperty
 };
